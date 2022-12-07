b9a7eb2c6fbfc975654afd50fe0fd2bdf297ace0
Amanda K <109555609+akuo1981@users.noreply.github.com>
2022-12-07 12:13:16 -0800

    Update fibonacci.py
    Updated instance variables naming

--- fibonacci.py
+++ fibonacci.py
@@ -1,22 +1,22 @@
 # Automation onboarding
 # Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144…
 
-def fibonacci(maxN):
+def fibonacci(max_number):
     total = 0
-    firstN = 0
-    secondN = 0
+    first_number = 0
+    second_number = 0
     print(total)
-    while total < maxN:
-        total = firstN + secondN
-        if total > maxN:
+    while total < max_number:
+        total = first_number + second_number
+        if total > max_number:
             break
         else:
             print(total)
-            firstN = secondN
-            if secondN == 0:
-                secondN = 1
+            first_number = second_number
+            if second_number == 0:
+                second_number = 1
             else:
-                secondN = total
+                second_number = total
 
 
 fibonacci(150)
